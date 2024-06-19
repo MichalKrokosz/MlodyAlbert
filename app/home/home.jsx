0d1b29d49099193380f0940da2ea64997b42c640
MichalKrokosz <michalkrokoszynski@gmail.com>
2024-06-19 10:20:23 +0200

    test pobierania na serwer

--- app/home/home.jsx
+++ app/home/home.jsx
@@ -1,9 +1,9 @@
 import Content from "../components/content/content"
 
 const description = [
-    {text: "Strona w fazie rozwoju"},
-    {text: "Strona cały czas się rozwija ale chcemy dla państwa przedstawić w miare możliwości oferę na następny rok szkolny"},
-    {text: "Przed pierwszym września stronna powinna być gotowa"}
+    {text: "Strona w fazie rozwoju."},
+    {text: "Strona cały czas się rozwija ale chcemy dla państwa przedstawić w miare możliwości oferę na następny rok szkolny."},
+    {text: "Przed pierwszym września stronna powinna być gotowa."}
 ]
 
 
